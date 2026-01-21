d3ed25733f0860aac8394bdbf0555d483d8cd313
Seyidakif101 <sayidakhifia-mpa101@code.edu.az>
2026-01-21 19:54:34 +0400

    Add project files.

---
+++ Nedi-Budi/Program.cs
@@ -0,0 +1,31 @@
+namespace Nedi_Budi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "areas",
+                pattern: "{area:exists}/{controller=Dasboard}/{action=Index}/{id?}");
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
